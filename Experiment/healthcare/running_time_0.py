b80180259f14bbee6da5e26f1c11896458357424
JinyangLi01 <jinyli@umich.edu>
2022-04-25 13:46:37 -0400

    healthcare data experiment

--- Experiment/healthcare/running_time_0.py
+++ Experiment/healthcare/running_time_0.py
@@ -11,20 +11,30 @@ import time
 from intbitset import intbitset
 import json
 
-from Algorithm import ProvenanceSearch_3_20220324 as ps
+from Algorithm import ProvenanceSearch_10_20220421 as ps
 from Algorithm import LatticeTraversal_2_2022405 as lt
 
 
 
 
-data_file = r"../../InputData/Pipelines/healthcare/before_selection.csv"
-selection_file = r"../../InputData/Pipelines/healthcare/selection1.json"
-
-minimal_refinements1, running_time1 = ps.FindMinimalRefinement(data_file, selection_file)
-
-minimal_refinements2, running_time2 = lt.FindMinimalRefinement(data_file, selection_file)
 
 
 
+data_file = r"../../InputData/Pipelines/healthcare/incomeK/before_selection_incomeK.csv"
+selection_file = r"../../InputData/Pipelines/healthcare/incomeK/selection4.json"
+
+print("========================== provenance search ===================================")
+minimal_refinements1, minimal_added_refinements1, running_time1 = ps.FindMinimalRefinement(data_file, selection_file)
+
+print("running time = {}".format(running_time1))
+
+print("========================== lattice traversal ===================================")
+
+minimal_refinements2, minimal_added_refinements2, running_time2 = lt.FindMinimalRefinement(data_file, selection_file)
+
+print("running time = {}".format(running_time2))
+
+
+print(*minimal_refinements1, sep="\n")
 
 
