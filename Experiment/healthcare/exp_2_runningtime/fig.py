318f6f56a15c9322424866a608b1174878fc5243
JinyangLi01 <jinyli@umich.edu>
2022-08-27 18:51:02 -0400

    healthcare running time

--- Experiment/healthcare/exp_2_runningtime/fig.py
+++ Experiment/healthcare/exp_2_runningtime/fig.py
@@ -14,17 +14,13 @@ sns.set_style("whitegrid")
 # sns.palplot(sns.color_palette("deep", 10))
 # sns.palplot(sns.color_palette("Paired", 9))
 
-color = ['C1', 'C3']
-label = ["PS", "LT"]
-
+color = ['C1', 'C0', 'C3']
+label = ["PS_provenance", "PS_searching", "LT"]
 f_size = (14, 10)
-
-
-
-
 x_list = list()
 x_naive = list()
-execution_timeps = list()
+execution_timeps1 = list()
+execution_timeps2 = list()
 execution_timelt = list()
 
 input_path = r'time.csv'
@@ -44,21 +40,26 @@ for line in Lines:
         break
     items = line.strip().split(',')
     x_list.append(items[0])
-    execution_timeps.append(float(items[1]))
-    if len(items) == 3:
-        execution_timelt.append(float(items[2]))
+    execution_timeps1.append(float(items[1]))
+    execution_timeps2.append(float(items[2]))
+    if len(items) == 4:
+        execution_timelt.append(float(items[3]))
     else:
         execution_timelt.append(0)
 
-print(x_list, execution_timeps, execution_timelt)
+print(x_list, execution_timeps1, execution_timeps2, execution_timelt)
 
 index = np.arange(len(x_list))
 bar_width = 0.35
 
 fig, ax = plt.subplots(1, 1, figsize=f_size)
 
-plt.bar(index, execution_timeps, bar_width, color=color[0], label=label[0])
-plt.bar(index + bar_width, execution_timelt, bar_width,  color=color[1], label=label[1])
+
+plt.bar(index, execution_timeps1, bar_width, color=color[0], label=label[0])
+plt.bar(index, execution_timeps2, bar_width, bottom=execution_timeps1,
+       color=color[1], label=label[1])
+
+plt.bar(index + bar_width, execution_timelt, bar_width,  color=color[2], label=label[2])
 plt.xticks(index + bar_width, x_list)
 
 plt.xlabel('Query and Constraint')
