628410d1cf730dc3644d15442ef8d4b5cfdf3c02
JinyangLi01 <jinyli@umich.edu>
2022-08-27 16:05:43 -0400

    adult scale

--- Experiment/adult/scale/scale_1.py
+++ Experiment/adult/scale/scale_1.py
@@ -16,11 +16,11 @@ from Algorithm import LatticeTraversal_2_2022405 as lt
 
 minimal_refinements1 = []
 minimal_added_refinements1 = []
-running_time1 = []
-
+# running_time1 = []
+#
 minimal_refinements2 = []
 minimal_added_refinements2 = []
-running_time2 = []
+# running_time2 = []
 
 
 time_limit = 5*60
@@ -29,9 +29,9 @@ time_limit = 5*60
 
 time_output_file = r"time.csv"
 time_output = open(time_output_file, "w")
-time_output.write("data size,PS,LT\n")
+time_output.write("data size,running time,provenance time,search time\n")
 
-result_output_file = r"./result.txt"
+result_output_file = r"result.txt"
 result_output = open(result_output_file, "w")
 result_output.write("selection file, result\n")
 
@@ -42,24 +42,22 @@ for i in range(1, 7):
 
     print("========================== provenance search ===================================")
     minimal_refinements1, running_time1, assign_to_provenance_num,\
-        provenance_time, table_time, search_time = \
+        provenance_time, search_time = \
         ps.FindMinimalRefinement(data_file, query_file, constraint_file, time_limit)
-
-    print("running time = {}".format(running_time1), provenance_time, table_time, search_time)
+    print("running time = {}".format(running_time1), provenance_time, search_time)
     print(*minimal_refinements1, sep="\n")
 
-
-    # print("========================== lattice traversal ===================================")
-    #
-    # minimal_refinements2, minimal_added_refinements2, running_time2 = \
-    #     lt.FindMinimalRefinement(data_file, query_file, constraint_file, time_limit)
-    # print("running time = {}".format(running_time2))
-    # if running_time2 < time_limit:
-    #     print(*minimal_refinements2, sep="\n")
-    # running_time2 = 0
+    print("========================== lattice traversal ===================================")
+    minimal_refinements2, minimal_added_refinements2, running_time2 = \
+        lt.FindMinimalRefinement(data_file, query_file, constraint_file, time_limit)
+    print("running time = {}".format(running_time2))
+    if running_time2 < time_limit:
+        print(*minimal_refinements2, sep="\n")
+    running_time2 = 0
     result_output.write("\n")
 
-    time_output.write("{},{:0.2f},{:0.2f},{:0.2f},{:0.2f}\n".format(2**(i-1), running_time1, provenance_time, table_time, search_time))
+    time_output.write("{},{:0.2f},{:0.2f},{:0.2f},{:0.2f}\n".format(2**(i-1), running_time1, provenance_time,
+                                                                    search_time, running_time2))
     result_output.write("{}\n".format(2**(i-1)))
     result_output.write(",".join(str(item) for item in minimal_added_refinements1))
     result_output.write("\n")
