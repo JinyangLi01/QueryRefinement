bf3ca98a0525893428d7a7764a27ae7563237172
Jinyang Li <jinyli@umich.edu>
2023-02-17 14:44:24 -0500

    1G query selectivity q3 refine

--- Experiment/TPCH/1G/query_selectivity/q3_refine/fig.py
+++ Experiment/TPCH/1G/query_selectivity/q3_refine/fig.py
@@ -7,8 +7,6 @@ import matplotlib.ticker as ticker
 import datetime
 from matplotlib.dates import AutoDateLocator, AutoDateFormatter, date2num
 
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
 
 sns.set_palette("Paired")
 # sns.set_palette("deep")
@@ -18,6 +16,9 @@ plt.rcParams['xtick.major.size'] = 20
 plt.rcParams['xtick.major.width'] = 4
 plt.rcParams['xtick.bottom'] = True
 plt.rcParams['ytick.left'] = True
+
+plt.rc('text', usetex=True)
+plt.rc('font', size=70, weight='bold')
 
 color = ['C1', 'C0', 'C3', 'C2']
 label = ['PS-prov', "PS-search", "BL-prov", "BL-search"]
@@ -64,7 +65,7 @@ def run(query, size, constraint):
     print(x_list, execution_timeps1, execution_timeps2)
 
     index = np.arange(len(execution_timeps1))
-    bar_width = 0.35
+    bar_width = 0.4
 
     fig, ax = plt.subplots(1, 1, figsize=f_size)
 
@@ -77,16 +78,16 @@ def run(query, size, constraint):
     plt.bar(index, execution_timeps1, bar_width, color=color[0], label=label[0])
     plt.bar(index, execution_timeps2, bar_width, bottom=execution_timeps1,
             color=color[1], label=label[1])
-    plt.bar(index + bar_width, execution_timebl1, bar_width, color=color[2], label=label[2])
-    plt.bar(index + bar_width, execution_timebl2, bar_width, bottom=execution_timebl1,
-            color=color[3], label=label[3])
-
-
-    plt.xticks(np.arange(0, 8, 2) + bar_width/2, x_list, rotation=20, fontsize=40)
+    # plt.bar(index + bar_width, execution_timebl1, bar_width, color=color[2], label=label[2])
+    # plt.bar(index + bar_width, execution_timebl2, bar_width, bottom=execution_timebl1,
+    #         color=color[3], label=label[3])
+    #
+    plt.xticks(np.arange(0, 8, 2) + bar_width/2, x_list, rotation=0, fontsize=70)
+    plt.yticks(fontsize=70, weight='bold')
 
     plt.xlabel(r'o\underline{ }orderdate')
-    plt.ylabel('Running time (s)')
-    plt.legend(loc='upper right', bbox_to_anchor=(1.03, 1.05))
+    # plt.ylabel('Running time (s)')
+    plt.legend(loc='upper right', bbox_to_anchor=(1.03, 0.55), fontsize=45)
     plt.tight_layout()
     fig_path = "query_selectivity_q" + str(query) + "_" + size + "_" + constraint + ".png"
 
@@ -94,4 +95,4 @@ def run(query, size, constraint):
     plt.show()
 
 
-run(3, "1G", "relax1")
+run(3, "1G", "refine1")
