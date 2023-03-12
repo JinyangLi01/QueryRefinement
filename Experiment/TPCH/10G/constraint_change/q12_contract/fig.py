b1a6bead9cd00ebb099e892b545ff78afe1d6989
Jinyang Li <jinyli@umich.edu>
2023-03-12 19:29:38 -0400

    10G constraint change q12 contract

--- Experiment/TPCH/10G/constraint_change/q12_contract/fig.py
+++ Experiment/TPCH/10G/constraint_change/q12_contract/fig.py
@@ -59,7 +59,7 @@ def run(query, size, constraint):
             execution_timebl1.append(0)
             execution_timebl2.append(0)
     # x_list = [19941230, 19950115, 19950130, 19950215, 19950230, 19950315, 19950330, 19950415]
-    x_list = ['115K', '125K',  '135K', '145K']
+    x_list = [40, 50, 60, 70, 80, 90]
 
     print(x_list, execution_timeps1, execution_timeps2)
 
@@ -81,11 +81,11 @@ def run(query, size, constraint):
     # plt.bar(index + bar_width, execution_timebl2, bar_width, bottom=execution_timebl1,
     #         color=color[3], label=label[3])
     #
-    plt.xticks(np.arange(0, 8, 2) + bar_width/2, x_list, rotation=0, fontsize=70)
+    plt.xticks(np.arange(0, 6), x_list, rotation=0, fontsize=70)
     plt.yticks(fontsize=70, weight='bold')
 
     plt.xlabel(r'\{l\underline{ }returnflag = A, \\o\underline{ }orderstatus=F\} $<=$', fontsize=65)
-    plt.legend(loc='upper right', bbox_to_anchor=(1.03, 1.02), fontsize=55)
+    plt.legend(loc='upper right', bbox_to_anchor=(0.65, 0.60), fontsize=55)
     # plt.legend(loc="best", fontsize=55)
     plt.tight_layout()
     fig_path = "constraint_change_q" + str(query) + "_" + size + "_" + constraint + ".png"
