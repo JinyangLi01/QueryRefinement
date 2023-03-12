b529270ed467ee7639590aef964700d03fbca985
Jinyang Li <jinyli@umich.edu>
2023-03-12 18:48:16 -0400

    fix figure

--- Experiment/TPCH/1G/constraint_change/q3_contract52/fig.py
+++ Experiment/TPCH/1G/constraint_change/q3_contract52/fig.py
@@ -84,8 +84,9 @@ def run(query, size, constraint):
     plt.xticks(np.arange(0, 6, 1), x_list, rotation=0, fontsize=70)
     plt.yticks(fontsize=70, weight='bold')
 
-    plt.xlabel(r'\{c\underline{ }nationkey = 2\} $<=$ (\%)', fontsize=70, weight='bold')
-    plt.legend(loc='upper right', bbox_to_anchor=(0.98, 0.55), fontsize=55)
+    plt.xlabel(r'\{l\underline{ }shipinstruct = \\COLLECT COD\} $<=$ (\%)', fontsize=60, weight='bold')
+
+    plt.legend(loc='upper right', bbox_to_anchor=(0.98, 0.6), fontsize=55)
     # plt.legend(loc="best")
     plt.tight_layout()
     fig_path = "constraint_change_q" + str(query) + "_" + size + "_" + constraint + ".png"
