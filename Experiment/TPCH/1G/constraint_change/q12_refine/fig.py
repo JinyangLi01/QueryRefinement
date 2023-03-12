c91575d04c80d4ecfc277258b08b89b51a6015fe
Jinyang Li <jinyli@umich.edu>
2023-03-12 15:45:53 -0400

    1G constraint change q12 refine

--- Experiment/TPCH/1G/constraint_change/q12_refine/fig.py
+++ Experiment/TPCH/1G/constraint_change/q12_refine/fig.py
@@ -60,7 +60,7 @@ def run(query, size, constraint):
             execution_timebl1.append(0)
             execution_timebl2.append(0)
     # x_list = [19941230, 19950115, 19950130, 19950215, 19950230, 19950315, 19950330, 19950415]
-    x_list = [2600, 3000, 3400, 3800]
+    x_list = [88, 90, 92, 94, 96, 98]
 
     print(x_list, execution_timeps1, execution_timeps2)
 
@@ -83,10 +83,10 @@ def run(query, size, constraint):
     #         color=color[3], label=label[3])
     #
 
-    plt.xticks(np.arange(0, 8, 2) + bar_width/2, x_list, rotation=0, fontsize=70)
+    plt.xticks(np.arange(0, 6), x_list, rotation=0, fontsize=70)
     plt.yticks(fontsize=70, weight='bold')
 
-    plt.xlabel(r'\{l\underline{ }returnflag = R, l\underline{ }shipinstruct =\\ TAKE BACK RETURN\} $>=$', fontsize=55)
+    plt.xlabel(r'\{l\underline{ }returnflag = R, l\underline{ }shipinstruct =\\ TAKE BACK RETURN\} $<=$ (\%)', fontsize=55)
     plt.legend(loc='upper right', bbox_to_anchor=(1, 0.56), fontsize=55)
     # plt.legend(loc="best")
     plt.tight_layout()
