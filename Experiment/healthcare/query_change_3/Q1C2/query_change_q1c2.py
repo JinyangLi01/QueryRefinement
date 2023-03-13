105a94156e93c45e5c9baf5f6c6b9f3df58ff212
Jinyang Li <jinyli@umich.edu>
2023-03-13 04:04:20 -0400

    healthcare query change q1c2

--- Experiment/healthcare/query_change_3/Q1C2/query_change_q1c2.py
+++ Experiment/healthcare/query_change_3/Q1C2/query_change_q1c2.py
@@ -10,9 +10,8 @@ import time
 from intbitset import intbitset
 import json
 
-from Algorithm import ProvenanceSearchValues_7_20220827 as ps
-from Algorithm import LatticeTraversal_4_20220901 as lt
-
+from Algorithm import ProvenanceSearchValues_8_20230119 as ps
+from Algorithm import LatticeTraversal_5_20230121 as lt
 
 minimal_refinements1 = []
 minimal_added_refinements1 = []
@@ -23,55 +22,65 @@ minimal_added_refinements2 = []
 running_time2 = []
 
 
-data_file = r"../../../../InputData/Healthcare/incomeK/before_selection_incomeK.csv"
-query_file_prefix = r"./query"
+data_file_prefix = r"../../../../InputData/Healthcare/incomeK/"
+query_file_prefix = r"query"
+time_limit = 30 * 60
 
-
-time_limit = 5*60
 
 def run_constraint(c):
     print("running query change constraint {}".format(c))
     constraint_file = r"./constraint" + str(c) + ".json"
 
-
-    time_output_file = r"./query_change_q1c2.csv"
+    time_output_file = r"./query_change_q1c" + str(c) + ".csv"
     time_output = open(time_output_file, "w")
     time_output.write("income,PS,LT\n")
 
-    result_output_file = r"./result_q1c2.txt"
+    result_output_file = r"./result_q1c1.txt"
     result_output = open(result_output_file, "w")
     result_output.write("selection file, result\n")
 
-    for i in range(1, 6):
+    for i in range(0, 8):
         print("query", i)
         query_file = query_file_prefix + str(i) + ".json"
         print("========================== provenance search ===================================")
-        minimal_refinements1, running_time1, assign_to_provenance_num, \
-        provenance_time, search_time = \
-            ps.FindMinimalRefinement(data_file, query_file, constraint_file, time_limit)
+        minimal_refinements1, running_time1, _, \
+            provenance_time1, search_time1 = \
+            ps.FindMinimalRefinement(data_file_prefix, separator, query_file, constraint_file, data_file_format,
+                                     time_limit)
 
         print("running time = {}".format(running_time1))
+        print(*minimal_refinements1, sep="\n")
 
-        running_time2 = 0
+        running_time2, provenance_time2, search_time2 = 0, 0, 0
         # print("========================== lattice traversal ===================================")
-        # minimal_refinements2, minimal_added_refinements2, running_time2 = \
-        #     lt.FindMinimalRefinement(data_file, query_file, constraint_file, time_limit)
+        # minimal_refinements2, minimal_added_refinements2, running_time2, provenance_time2, search_time2 = \
+        #     lt.FindMinimalRefinement(data_file_prefix, separator, query_file, constraint_file, data_file_format,
+        #                              time_limit)
         # if running_time2 > time_limit:
         #     print("naive alg out of time with {} time limit".format(time_limit))
         # else:
         #     print("running time = {}".format(running_time2))
-        print(*minimal_refinements1, sep="\n")
+        # print(*minimal_refinements2, sep="\n")
+
         result_output.write("\n")
-        idx = i * 50
-        time_output.write("{},{:0.2f},{:0.2f},{:0.2f}\n".format(idx, running_time1, provenance_time,
-                                                                search_time))
+        idx = i
+        if running_time2 < time_limit:
+            time_output.write("{},{:0.5f},{:0.5f},{:0.5f},"
+                              "{:0.5f},{:0.5f},{:0.5f}\n".format(idx, running_time1, provenance_time1, search_time1,
+                                                                 running_time2, provenance_time2, search_time2))
+        else:
+            time_output.write("{},{:0.5f},{:0.5f},{:0.5f},,,\n".format(idx, running_time1, provenance_time1,
+                                                                       search_time1))
         result_output.write("{}\n".format(idx))
-        result_output.write(", ".join(str(item) for item in minimal_added_refinements1))
+        result_output.write(",".join(str(item) for item in minimal_added_refinements1))
         result_output.write("\n")
         result_output.write("\n".join(str(item) for item in minimal_refinements1))
         result_output.write("\n")
     result_output.close()
     time_output.close()
 
+
+separator = ','
+data_file_format = ".csv"
 
 run_constraint(2)
