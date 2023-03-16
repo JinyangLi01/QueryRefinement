c702a797e8b5e64acdac1ad2d6d8c5c31b30644a
Jinyang Li <jinyli@umich.edu>
2023-03-15 22:39:37 -0400

    fairness aware fairness 1d

---
+++ Experiment/related_work_comparison/fairness_aware/fairness_1d/fairness_1d.py
@@ -0,0 +1,46 @@
+import copy
+from typing import List, Any
+import numpy as np
+import pandas as pd
+import time
+from intbitset import intbitset
+import json
+
+from Algorithm import ProvenanceSearchValues_8_predicate_on_same_attribute as ps
+from Algorithm import LatticeTraversal_5_20230121 as lt
+
+
+def JacardSimilarity(set1, set2):
+    return len(set1 & set2) / len(set1 | set2)
+
+
+data_file_prefix = r"../data/"
+query_file_prefix = r"./q"
+constraint_file_prefix = r"./"
+time_limit = 60 * 20
+separator = ' '
+
+time_output_prefix = r"./result_"
+
+c = 'refine11'
+constraint_file = r"./constraint_" + c + ".json"
+
+time_output_file = r"./query_change_q1_" + c + ".csv"
+time_output = open(time_output_file, "w")
+time_output.write("_,PS,PS_prov,PS_search,base,base_prov,base_search\n")
+
+result_output_file = r"./result_query_change_q1" + c + ".txt"
+result_output = open(result_output_file, "w")
+result_output.write("selection file, result\n")
+
+i = 1
+data_format = ".csv"
+print("query", i)
+query_file = query_file_prefix + str(i) + ".json"
+print("========================== provenance search ===================================")
+minimal_refinements1, running_time1, _, \
+    provenance_time1, search_time1 = \
+    ps.FindMinimalRefinement(data_file_prefix, separator, query_file, constraint_file, data_format, time_limit)
+
+print("running time = {}".format(running_time1))
+print(*minimal_refinements1, sep="\n")
